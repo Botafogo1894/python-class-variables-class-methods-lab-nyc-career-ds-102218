7f67edfc4f786db8a42071065af8a01989448926
Botafogo1894 <pruchka@gmail.com>
2018-10-30 15:40:52 -0400

    Finished

--- driver.py
+++ driver.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 class Driver:
     _all = []
     _count = 0
@@ -22,17 +24,21 @@ class Driver:
         return list(map(lambda item: item.car_make, Driver._all))
 
     @classmethod
+    def fleet_models(cls):
+        return list(map(lambda item: item.car_model, Driver._all))
+
+    @classmethod
     def fleet_makes_count(cls):
-        empty = {}
-        for item in Driver._all:
-            if item.car_make in empty.keys():
-                empty[item.car_make] +=  1
-            else:
-                empty[item.car_make] = 1
-        return empty
+        return Counter(Driver.fleet_makes())
 
+    @classmethod
+    def fleet_models_count(cls):
+        return Counter(Driver.fleet_models())
 
-
+    @classmethod
+    def percent_of_fleet(cls, word):
+        result = round((Driver.fleet_makes_count()[word])/float((Driver._count)), 2)
+        return str(result*100) + "%"
 
     @property
     def name(self):
