ef4ea52c69a34188a0acb42b806e5142e6053ff7
Botafogo1894 <pruchka@gmail.com>
2018-10-30 15:00:13 -0400

    lots of progress

---
+++ dilyan_solutions.py
@@ -0,0 +1,9 @@
+from driver import Driver
+
+d1 = Driver("Helga Pataki", "Toyota", "Camry")
+d2 = Driver("Arnold Shortman", "Toyota", "Highlander")
+d3 = Driver("Gerald Johanssen", "Toyota", "Camry")
+d4 = Driver("Robert 'Big Bob' Pataki", "Honda", "Pilot")
+d5 = Driver("Grandpa Phil", "Jeep", "Grand Cherokee")
+d6 = Driver("Rhonda Wellington Lloyd", "Kia", "Sonata")
+d7 = Driver("Phoebe Heyerdahl", "Honda", "Civic")
--- driver.py
+++ driver.py
@@ -2,4 +2,46 @@ class Driver:
     _all = []
     _count = 0
 
-    
+    def __init__(self, name, car_make, car_model):
+        self._name = name
+        self._car_make = car_make
+        self._car_model = car_model
+        Driver._all.append(self)
+        Driver._count += 1
+
+    @classmethod
+    def fleet_size(cls):
+        return Driver._count
+
+    @classmethod
+    def driver_names(cls):
+        return list(map(lambda item: item.name, Driver._all))
+
+    @classmethod
+    def fleet_makes(cls):
+        return list(map(lambda item: item.car_make, Driver._all))
+
+    @classmethod
+    def fleet_makes_count(cls):
+        empty = {}
+        for item in Driver._all:
+            if item.car_make in empty.keys():
+                empty[item.car_make] +=  1
+            else:
+                empty[item.car_make] = 1
+        return empty
+
+
+
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def car_make(self):
+        return self._car_make
+
+    @property
+    def car_model(self):
+        return self._car_model
